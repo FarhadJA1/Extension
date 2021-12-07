cd5e9017fdb4d66fef2ff4db1b2a1f07497ca1cb
FarhadJA1 <farhadja@code.edu.az>
2021-12-08 01:14:28 +0400

    initial

---
+++ String Check Extension/String Check Extension/Helpers/ExtensioMethods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace String_Check_Extension.Helpers
+{
+    public static class ExtensioMethods
+    {
+        public static int StringCheck(this string text, string pattern)
+        {
+            int count = 0;
+            if (Regex.IsMatch (text, pattern))
+            {
+                count++;
+            }
+
+            return count;
+
+
+
+
+
+
+           
+          
+        }
+    }
+}
---
+++ String Check Extension/String Check Extension/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static String_Check_Extension.Helpers.ExtensioMethods;
+namespace String_Check_Extension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Salam Cavid. Necəsən Cavid";
+            Console.WriteLine(text.StringCheck(@"Cavid"));
+        }
+
+    }
+}
